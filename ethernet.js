0028215bcaddd4a6f0f467d6d71efe5e0e3a62b9
Clémence Fernandez <clemencefrz@gmail.com>
2025-12-27 10:35:44 +0100

    SFD et destination address

--- ethernet.js
+++ ethernet.js
@@ -5,30 +5,64 @@ import { Buffer } from 'node:buffer';
 
 const preambleSize = 7 // Pour savoir qu'il s'agit d'un message éthernet
 const SFDSize = 1
-const destinationAddressSize = 6
+const MACAddressSize = 6
 const sourceAddressSize = 6
 const lengthTypeSize = 2
 const macClientDataSize = 1500 // Vraie donnée qu'on veut envoyer
 // const padSize = ??
 const frameCheckSequenceSize = 4
-const messageEthernetDataFrameSize = preambleSize + SFDSize + destinationAddressSize + sourceAddressSize + lengthTypeSize + macClientDataSize + frameCheckSequenceSize
+const messageEthernetDataFrameSize = preambleSize + SFDSize + MACAddressSize + sourceAddressSize + lengthTypeSize + macClientDataSize + frameCheckSequenceSize
 
 
 const preambleOctet = 0b10101010  // cf page 129 du PDF
 
+
+/**
+ * @typedef {Buffer} MACAddress
+ */
+
+/**
+ * @param {any} x
+ * @returns {x is MACAddress}
+ */
+function estMACAdress(x) {
+    if (!Buffer.isBuffer(x)) {
+        return false
+    }
+    if (x.size !== MACAddressSize) {
+        return false
+    }
+    return true;
+}
+
 // Créer le message à envoyer
 // page 107 du pdf
-
 /**
  * Envoie le message encapsulé dans une frame éthernet
  * @param {Buffer} macClientData
+ * @param {MACAddress} destinationAddress // doit faire 6 octets p109
  * @returns {Buffer}
  */
-export function wrapEthernet(macClientData) {
+export function wrapEthernet(macClientData, destinationAddress) {
     const messageEthernetDataFrame = Buffer.alloc(messageEthernetDataFrameSize)
+
+    // Préambule
     for (let i=0; i < preambleSize; i++) {
         messageEthernetDataFrame[i] = preambleOctet;
     }
+
+    // SFD
+    messageEthernetDataFrame[preambleSize] = 0b10101011
+
+    // Destination address
+    if (!estMACAdress(destinationAddress)) {
+        throw new Error(`Le format de l'adresse de destination est incorrect : ${destinationAddress}`)
+    }
+    destinationAddress.copy(messageEthernetDataFrame, preambleSize + SFDSize)
+
+    
+    
+
     return messageEthernetDataFrame
 }
 
--- exercice.md
+++ exercice.md
@@ -2,9 +2,10 @@
 
 étape 1 - Créer les deux process (fichiers emetteur.js, recepteur.js) qui se parlent via UDP Broadcast (sur 0.0.0.0)
 étape 2 - Réimplementer un message éthernet
-ressource : page 53 de https://www.my-wire.de/schnittstellenbeschreibung/ethernet.pdf
+ressource : p107 https://www.my-wire.de/schnittstellenbeschreibung/ethernet.pdf
 
 TO DO : 
 - Finir le dataframe et l'extraire vraiment (wrapEthernet, unwrapEthernet)
+- Recoder ARP
 - Implémenter le système de vérification (crc)
 - Refaire la même chose en Rust
